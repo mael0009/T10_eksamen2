b56c3c7e02e2b1364045bb93b056066d90c37381
Manar <mael0009@stud.kea.dk>
2025-12-06 15:47:27 +0100

    header mobil + fonts

---
+++ header.js
@@ -0,0 +1,7 @@
+
+const burgerBtn = document.getElementById("burger-btn");
+const mobilNav = document.getElementById("mobil-nav");
+
+burgerBtn.addEventListener("click", () => {
+    mobilNav.classList.toggle("show");
+});
