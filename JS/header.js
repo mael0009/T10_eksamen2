ab9e5154e3ecc0fdc25929b6403469d6649f093d
Manar <mael0009@stud.kea.dk>
2025-12-17 09:40:00 +0100

    justeringer ift. feedback + fælles spacing + ændring på footer

---
+++ JS/header.js
@@ -0,0 +1,10 @@
+
+// HEADER //
+const burgerBtn = document.getElementById("burger-btn");
+const mobilNav = document.getElementById("mobil-nav");
+
+burgerBtn.addEventListener("click", () => {
+    mobilNav.classList.toggle("show");
+});
+
+
